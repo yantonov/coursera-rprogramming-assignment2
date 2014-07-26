ccc91854e5dd2c0abd739d6744f559b2ea2314d2
Yury Antonov <yantonov@yandex.ru>
2014-07-27 02:00:37 +0600

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+## function returns list of named functions
+## these functions allow you to get and set matrix value using lexical clojures
+## and get and set (cache) inverse matrix
     inverseMatrix <- NULL
     set <- function(y) {
         x <<- y
@@ -24,10 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inverseValue <- x$getInverseMatrix()
+    ## get inverse matrix or null if it is not calculated yet
     if(!is.null(inverseValue)) {
+        ## if inverse matrix is already calculated returns it
         message("getting cached data")
         return(inverseValue)
     }
+    ## if inverse value is not calculated yet, calculate it and cache
+    ## and returns calculated value
     data <- x$get()
     inverseValue <- solve(data)
     x$setInverseMatrix(inverseValue)
